3c2fcc4fd4f37c95840706dc94a8a52a96a1d107
Rodrigo <rlvenancio@gmail.com>
2022-03-25 22:11:16 -0300

    Adiciona laço de repetição While ao jogo

--- pythonProject/adivinhacao.py
+++ pythonProject/adivinhacao.py
@@ -1,23 +1,1 @@
-print("*********************************")
-print("Bem vindo no jogo de Adivinhação!")
-print("*********************************")
-
-numero_secreto = 42
-
-chute = int(input("Digite o seu numero: "))
-
-print("Você digitou",chute)
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if (acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o numero secreto.")
-    elif (menor): # Só executa o ELIF caso o a condição do IF seja falsa
-        print("Você errou! O seu chute foi menor do que o numero secreto.")
-
-print("Fim do jogo!")
+print("*********************************")print("Bem vindo no jogo de Adivinhação!")print("*********************************")numero_secreto = 42total_de_tentativas = 3rodada = 1while(rodada <= total_de_tentativas):    print("Tentativa {} de {}".format(rodada,total_de_tentativas))    chute = int(input("Digite o seu numero: "))    print("Você digitou",chute)    acertou = chute == numero_secreto    maior = chute > numero_secreto    menor = chute < numero_secreto        if (acertou):        print("Você acertou")    else:        if(maior):            print("Você errou! O seu chute foi maior do que o numero secreto.")        elif (menor): # Só executa o ELIF caso o a condição do IF seja falsa            print("Você errou! O seu chute foi menor do que o numero secreto.")    rodada = rodada + 1print("Fim do jogo!")
