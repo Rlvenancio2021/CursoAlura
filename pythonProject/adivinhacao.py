3d95f78c07b1c528582189f7305a38617bdfc0ef
Rodrigo <rlvenancio@gmail.com>
2022-03-30 20:57:39 -0300

    Adiciona pontuação

--- pythonProject/adivinhacao.py
+++ pythonProject/adivinhacao.py
@@ -6,6 +6,7 @@ print("********************************")
 
 numero_secreto = random.randrange(1, 101) # random.randrange() gera um número aleatório entre 0 e 100
 total_de_tentativas = 0
+pontos =1000
 
 # INSERI NÍVEIS DE PONTUAÇÃO #
 print("Qual nível de dificuldade?")
@@ -20,6 +21,7 @@ elif nivel == 2:
 else:
     total_de_tentativas = 5
 
+# LÓGICA DO JOGO #
 for rodada in range(1, total_de_tentativas + 1): # total_de_tentativas + 1 para garantir que tenhamos a 3 tentativas,
     #caso contrário quando a loop chegar no 3 ele irá parar sem executar.
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -34,12 +36,14 @@ for rodada in range(1, total_de_tentativas + 1): # total_de_tentativas + 1 para 
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertou e fez {} pontos".format(pontos))
         break # Encerra a execução do programa caso a condição for tree.
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
+        pontos_perdidos = abs(numero_secreto - chute) # função ABS retorna números absolutos
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
