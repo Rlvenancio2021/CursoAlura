e84db0c7f89b122c60306971d04da7c7ab4461b9
Rodrigo <rlvenancio@gmail.com>
2022-03-31 10:59:01 -0300

    Corrigi problema de execução

--- pythonProject/adivinhacao.py
+++ pythonProject/adivinhacao.py
@@ -53,3 +53,8 @@ def jogar_adivinhacao():
             pontos = pontos - pontos_perdidos
 
     print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar_adivinhacao()
+# Quando executampos em linha da comando pelo "python3 adivinhacao.py", por traz o python cria uma variável e atribui
+# um valor a ela, porém quando importado ele não seta a variável
--- pythonProject/forca.py
+++ pythonProject/forca.py
@@ -4,3 +4,8 @@ def jogar_forca():
     print("********************************")
 
     print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar_forca()
+# Quando executampos em linha da comando pelo "python3 adivinhacao.py", por traz o python cria uma variável e atribui
+# um valor a ela, porém quando importado ele não seta a variável
