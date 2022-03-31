e861dd365c08efc2ce34ea778ee799cd9a916751
Rodrigo <rlvenancio@gmail.com>
2022-03-30 22:18:50 -0300

    Cria programa jogos para seleção de jogos

---
+++ pythonProject/jogos.py
@@ -0,0 +1,17 @@
+import forca
+import adivinhacao
+
+print("********************************")
+print("***** Escolha o seu jogo! ******")
+print("********************************")
+
+print("(1) Forca (2) Adivinhação")
+
+jogo = int(input("Qual jogo? "))
+
+if jogo == 1:
+    print("Jogando Forca")
+    forca.jogar_forca()
+elif jogo == 2:
+    print("jogando Adivinhação")
+    adivinhacao.jogar_adivinhacao()
