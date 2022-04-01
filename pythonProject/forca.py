48b6a6fd4732aaaee9ca29a479e6a42bc29753c7
Rodrigo <rlvenancio@gmail.com>
2022-03-31 21:09:08 -0300

    Adiciona verificador de erros e acertos

--- pythonProject/forca.py
+++ pythonProject/forca.py
@@ -3,26 +3,36 @@ def jogar_forca():
     print("*** Bem vindo ao jogo forca! ***")
     print("********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
-        chute = input("Qual letra? ").strip()
+        chute = input("Qual letra? ").strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-               letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta: # Condição criada para teste e incremento do erro, caso ocorra.
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                   letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6 # Verificar se atingiu o total de tentativas
+        acertou = "_" not in letras_acertadas # Verificar o conteúdo das letras acertadas
         print(letras_acertadas)
 
+    if acertou:
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 if __name__ == "__main__":
