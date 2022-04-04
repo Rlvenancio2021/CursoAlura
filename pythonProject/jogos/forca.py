5b26cc655c1b7b15f9c7aa6496849385b5ff05f7
Rodrigo <rlvenancio@gmail.com>
2022-04-04 16:47:18 -0300

    Adiciona função de leitura de arquvio

--- pythonProject/jogos/forca.py
+++ pythonProject/jogos/forca.py
@@ -1,9 +1,22 @@
+import random
+
 def jogar_forca():
     print("********************************")
     print("*** Bem vindo ao jogo forca! ***")
     print("********************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip() # função para tirar o caracter "\n" da string que irá retornar.
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta] # automatiza o preenchimento inicial da lista,
     # este formato chama-se List Comprehension / Compreensão de Lista.
 
