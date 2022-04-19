5596da654e5b25af0541116a05af0098824f37d5
Rodrigo <rlvenancio@gmail.com>
2022-04-18 22:59:46 -0300

    Adiciona método estático

--- pythonProject2/OO/conta.py
+++ pythonProject2/OO/conta.py
@@ -14,23 +14,32 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    # MÉTODO PRIVADO # só é acessado dentro da classe, porém pode ser acessado de forma direta
+    def __pode_sacar(self, valor_a_sacar): # Com uderscore duplo, tranformamos o MÉTODO em privado
+        valor_disponivel_a_sacar = self.saldo + self.limite
+        return valor_a_sacar <= valor_disponivel_a_sacar # retorna True ou False
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} ultrapassa o limite permitido.")
 
     def transfere(self, valor, destino):
         self.saca(valor) # O self permite acesso tanto o atributo como um método dentro da classe, dessa forma
                          # a conta origem é a conta que chamar o método "transfere"
         destino.deposita(valor)
 
-    # Métodos GETTERS (obter) e SETTERS (alterar) #
-    def get_saldo(self):
-        return self.__saldo
-
-    def get_titular(self):
-        return self.__titular
-
     # PROPRIEDADE #
     # Uma outra forma de adicionar GETTERS e SETTERS
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
     @property # GET
     def limite(self):
         return self.__limite
@@ -38,3 +47,12 @@ class Conta:
     @limite.setter # SET
     def limite(self, limite):
         self.__limite = limite
+
+    # MÉTODO ESTÁTICO #
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_banco():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237', 'Itaú':'341'}
