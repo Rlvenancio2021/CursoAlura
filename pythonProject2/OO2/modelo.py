c6149c5f8aece3df949b3154325a5b02e44b3f64
Rodrigo <rlvenancio@gmail.com>
2022-04-19 21:10:15 -0300

    Adiciona representação Textual de Objetos

--- pythonProject2/OO2/modelo.py
+++ pythonProject2/OO2/modelo.py
@@ -20,8 +20,9 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.likes} Likes')
+    # Representação Textual de Objetos #
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.likes} Likes'
 
     # CLASSE HERDEIRA (SUBCLASSES) #
 class Filme(Programa):
@@ -29,16 +30,16 @@ class Filme(Programa):
         super().__init__(nome, ano) # Utilizamos o Super para chamar o inicializador da classe mãe (tbm conhecida como super)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes')
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.temporadas} temporadas -  {self.likes} Likes')
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.temporadas} temporadas -  {self.likes} Likes'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160) # instancia o objeto
 vingadores.dar_like()
@@ -51,4 +52,4 @@ atlanta.dar_like()
 filmes_e_serias = [vingadores, atlanta]
 
 for programa in filmes_e_serias: # Corre a nossa lista e retorna o conteúdo independente da classe
-    programa.imprime() # imprime o resultado conforme o método específico de cada classe
+    print(programa) # retorno conforme o método __str__ (trata-se de um código mais pythônico)
